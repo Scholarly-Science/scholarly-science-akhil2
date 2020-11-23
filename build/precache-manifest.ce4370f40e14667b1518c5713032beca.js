08c0696d613f6e6575aa6952ee376302f33a3afb
DhruvGodambe <dhruvrockstone@gmail.com>
2020-11-23 19:50:28 +0530

    css change

---
+++ build/precache-manifest.ce4370f40e14667b1518c5713032beca.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feee25c54dec1f89540ef607bd37695c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f40daaae212c30cb4635",
+    "url": "/static/css/2.6cd19327.chunk.css"
+  },
+  {
+    "revision": "0eb5710a10433c831705",
+    "url": "/static/css/main.882019df.chunk.css"
+  },
+  {
+    "revision": "f40daaae212c30cb4635",
+    "url": "/static/js/2.e318e200.chunk.js"
+  },
+  {
+    "revision": "09fff3253561b749be154113fd7383cc",
+    "url": "/static/js/2.e318e200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb5710a10433c831705",
+    "url": "/static/js/main.5baef940.chunk.js"
+  },
+  {
+    "revision": "f6051be2a894ab18a75c",
+    "url": "/static/js/runtime-main.ef9407ac.js"
+  },
+  {
+    "revision": "d0a7a441a0be04a7afbfc8a9964bdd05",
+    "url": "/static/media/2.d0a7a441.svg"
+  },
+  {
+    "revision": "4c83f102654e013379d8372275d6ddcf",
+    "url": "/static/media/3.4c83f102.svg"
+  },
+  {
+    "revision": "4d31711f9afaf218f53846d041ec2938",
+    "url": "/static/media/4.4d31711f.svg"
+  },
+  {
+    "revision": "3c71c3fcf33f235f1dd9c3df7516de70",
+    "url": "/static/media/Asset 1.3c71c3fc.svg"
+  },
+  {
+    "revision": "d119f263edb90d4c505873eefb72b8fc",
+    "url": "/static/media/Group 120.d119f263.svg"
+  },
+  {
+    "revision": "bbafb9dc68571583d7a861edc502728f",
+    "url": "/static/media/Group 122.bbafb9dc.svg"
+  },
+  {
+    "revision": "abde4201a6e3def15604465ff7a9b630",
+    "url": "/static/media/ai-card.abde4201.svg"
+  },
+  {
+    "revision": "4d2fb5078383c2dea3c97c8ec3bccb64",
+    "url": "/static/media/ai-modal.4d2fb507.svg"
+  },
+  {
+    "revision": "4f2bb5c433957e5aa781aa5fe93f21e1",
+    "url": "/static/media/auditing-card.4f2bb5c4.svg"
+  },
+  {
+    "revision": "fc0d7ae33f0c4a1a11450a7e03e7eb55",
+    "url": "/static/media/auditing-modal.fc0d7ae3.svg"
+  },
+  {
+    "revision": "dc3d4dc4226ac2f534268ad37aa4b907",
+    "url": "/static/media/box.dc3d4dc4.svg"
+  },
+  {
+    "revision": "cd56ca7e722009a228400afb68dfd098",
+    "url": "/static/media/companies.cd56ca7e.svg"
+  },
+  {
+    "revision": "bb4f3c6116af9ce36e3b37afa864dbd9",
+    "url": "/static/media/development.bb4f3c61.svg"
+  },
+  {
+    "revision": "a74a3df64fee195374a0ce2d95e42830",
+    "url": "/static/media/ecosystem.a74a3df6.svg"
+  },
+  {
+    "revision": "b73011e90f16d0d9eef5803ae0946277",
+    "url": "/static/media/error.b73011e9.svg"
+  },
+  {
+    "revision": "799b8d1a87764697ea7bc4acaf853019",
+    "url": "/static/media/first-sec-bg.799b8d1a.svg"
+  },
+  {
+    "revision": "81ad048ed858673aaca6cc2227076b8a",
+    "url": "/static/media/icons8-google.81ad048e.svg"
+  },
+  {
+    "revision": "970e3faeee14480e40180f52fbc614d9",
+    "url": "/static/media/icons8-home.970e3fae.svg"
+  },
+  {
+    "revision": "d849bc2557228a905cb46a21f1819d90",
+    "url": "/static/media/icons8-linkedin.d849bc25.svg"
+  },
+  {
+    "revision": "5fab2872d44112304a841b9b8c339914",
+    "url": "/static/media/mobile-bg-414.5fab2872.svg"
+  },
+  {
+    "revision": "6a9c49c84a1e2763a96629136ef6919a",
+    "url": "/static/media/mobile-bg-pixel-Xl.6a9c49c8.svg"
+  },
+  {
+    "revision": "89d88fd442393f0a7116eed02b4ffd0c",
+    "url": "/static/media/modal-bottom.89d88fd4.png"
+  },
+  {
+    "revision": "eb2ccfcf6006904f864bcdd361a29841",
+    "url": "/static/media/modal-logo.eb2ccfcf.svg"
+  },
+  {
+    "revision": "c601db7b845e383470f50125a7253bd8",
+    "url": "/static/media/modal-mobile.c601db7b.png"
+  },
+  {
+    "revision": "bf4884ab2fc002c05b4f5c648efe9d9a",
+    "url": "/static/media/negative-card.bf4884ab.svg"
+  },
+  {
+    "revision": "7955e3eceaf12c2071b365a91d7e5bd6",
+    "url": "/static/media/negative-modal.7955e3ec.svg"
+  },
+  {
+    "revision": "d71186278f7cba09f2877735dcf0633d",
+    "url": "/static/media/orig-1.d7118627.svg"
+  },
+  {
+    "revision": "627e5aecfe1b9fc96ccb47a4d5b9120f",
+    "url": "/static/media/plan.627e5aec.png"
+  },
+  {
+    "revision": "8ac1f1c8f67d15983dd83a1cb6be5e46",
+    "url": "/static/media/voice-card.8ac1f1c8.svg"
+  },
+  {
+    "revision": "ac8a753ee7923837868bf08918b57743",
+    "url": "/static/media/voice-modal.ac8a753e.svg"
+  },
+  {
+    "revision": "4dc7071add76c3a16cc2946249d58e41",
+    "url": "/static/media/work.4dc7071a.svg"
+  }
+]);
